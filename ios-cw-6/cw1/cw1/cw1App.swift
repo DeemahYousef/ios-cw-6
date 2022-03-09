e0aa8d18245b5a76e69bae3d7e2b0bae175e8f12
DeemahYousef <dalkhaledi12345@hotmail.com>
2022-03-09 18:01:23 +0300

    done

---
+++ ios-cw-6/cw1/cw1/cw1App.swift
@@ -0,0 +1,17 @@
+//
+//  cw1App.swift
+//  cw1
+//
+//  Created by DIMA ALKHALEDI on 09/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct cw1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
